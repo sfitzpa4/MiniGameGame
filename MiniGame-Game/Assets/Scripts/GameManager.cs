3e0532dae1d731ca1984eb3dc9787f1a5d5d7471
sfitzpa4 <sfitzpa4@ycp.edu>
2017-01-31 18:33:14 -0500

    Added lives, particles, colors etc

--- MiniGame-Game/Assets/Scripts/GameManager.cs
+++ MiniGame-Game/Assets/Scripts/GameManager.cs
@@ -2,59 +2,84 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
 	public int lives = 3;
 	public int bricks = 20;
 	public float resetDelay = 1f;
+	public Text livesText;
+	
+	public GameObject gameOver;
+	public GameObject winner;
+	public GameObject brickParticle;
 	public GameObject paddle;
-	public GameObject bricksPrefab;
+	public GameObject bricksModel;
+	
 	public static GameManager instance = null;
-
+	
 	private GameObject clonePaddle;
-
+	
 	// Use this for initialization
-	void Awake () {
-		if (instance == null)
+	void Start () {
+		if (instance == null) {
 			instance = this;
-		else if (instance != this)
-			Destroy (gameObject);
-		Setup ();
-	}
-
-	public void Setup()
-	{
-		clonePaddle = Instantiate (paddle, transform.position, Quaternion.identity) as GameObject;
-		Instantiate (bricksPrefab, transform.position, Quaternion.identity);
-	}
-
-	void IsGameOver()
-	{
-	}
-
-	void Reset()
-	{
-		Time.timeScale = 1f;
-		Application.LoadLevel (Application.loadedLevel);
+		}
+		else if (instance != this) {
+			Destroy(gameObject);
+		}
+		Setup();
 	}
 	
-	public void LoseLife()
+	// Update is called once per frame
+	void Setup () {
+		clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+		Instantiate(bricksModel, transform.position, Quaternion.identity);
+	}
+	
+	void CheckGameOver() {
+	
+		if (bricks < 1)
+		{
+			winner.SetActive(true);
+			Time.timeScale = 0.25f;
+			Invoke("Reset", resetDelay);
+		}
+		
+		if (lives < 1) 
+		{
+			gameOver.SetActive(true);
+			Time.timeScale = 0.25f;
+			Invoke("Reset", resetDelay);
+		}
+	}
+	
+	void Reset() {
+		Time.timeScale = 1f;
+		// Return to first level (could be changed for new levels)
+		Scene scene = SceneManager.GetActiveScene();
+		SceneManager.LoadScene(scene.name);
+	}
+	
+	public void LoseLife() 
 	{
 		lives--;
-		Destroy (clonePaddle);
+		livesText.text = "Lives: " + lives;
+		Instantiate(brickParticle, clonePaddle.transform.position, Quaternion.identity);
+		Destroy(clonePaddle);
 		Invoke ("SetupPaddle", resetDelay);
-		IsGameOver ();
+		CheckGameOver();
 	}
-
+	
+	void SetupPaddle() 
+	{
+		clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+	}
+	
 	public void DestroyBrick()
 	{
 		bricks--;
-		IsGameOver ();
-	}
-
-	void SetupPaddle()
-	{
-		clonePaddle = Instantiate (paddle, transform.position, Quaternion.identity) as GameObject;
+		CheckGameOver();
 	}
 }
