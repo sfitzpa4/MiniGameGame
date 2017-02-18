f5125ccb458c8e327688d6e1811a040cccd37001
sfitzpa4 <sfitzpa4@ycp.edu>
2017-02-18 16:12:58 -0500

    Added unity scene & scripts

---
+++ MiniGame-Game/Assets/Scripts/AccelerometerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerometerInput : MonoBehaviour {
+
+    public float speed = 1f;
+    private Rigidbody rb;
+
+    void Awake ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = new Vector3(Input.acceleration.x, 0.0f, -Input.acceleration.z);
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ MiniGame-Game/Assets/Scripts/BrickCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickCollision : MonoBehaviour {
+
+	public GameObject bricksParticle;
+	
+	void OnCollisionEnter(Collision other) {
+			Instantiate(bricksParticle, transform.position, Quaternion.identity);
+			GameManager.instance.DestroyBrick();
+			Destroy(gameObject);
+	}
+}
