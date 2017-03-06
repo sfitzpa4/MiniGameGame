900dd4417b8fb8b0cab7f446394869c506f7eddf
sfitzpa4 <sfitzpa4@ycp.edu>
2017-03-06 01:02:05 -0500

    Added touch movement to Block Breaker

--- MiniGame-Game/Assets/Scripts/Paddle.cs
+++ MiniGame-Game/Assets/Scripts/Paddle.cs
@@ -11,8 +11,22 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float xPos = transform.position.x +(Input.GetAxis("Horizontal") * paddleSpeed);
-		playerPos = new Vector3 (Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);
-		transform.position = playerPos;
+
+		foreach (Touch touch in Input.touches) {
+			if (touch.position.x < Screen.width/2) {
+				
+				float xPos = transform.position.x + (-paddleSpeed);
+				playerPos = new Vector3 (Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);
+				transform.position = playerPos;
+
+
+			}
+			else if (touch.position.x > Screen.width/2) {
+				float xPos2 = transform.position.x +(paddleSpeed);
+				playerPos = new Vector3 (Mathf.Clamp(xPos2, -8f, 8f), -9.5f, 0f);
+				transform.position = playerPos;
+
+			}
+		}
 	}
 }
