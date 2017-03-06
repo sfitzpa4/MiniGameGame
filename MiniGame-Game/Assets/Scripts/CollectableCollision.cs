c9e0ac0586ceaf957425e990885227e605c0dc0c
Cooper Luetje <cluetje@ycp.edu>
2017-03-05 23:34:01 -0500

    Added Main Menu

---
+++ MiniGame-Game/Assets/Scripts/CollectableCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableCollision : MonoBehaviour {
+
+    // Use this for initialization
+    void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ MiniGame-Game/Assets/Scripts/DeadZone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col)
+    {
+        GameManager.instance.LoseLife();
+    }
+}
